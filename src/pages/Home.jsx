ec5a2dbdd8a1156f085f8e0a190ed4e53d067dea
HrithikSaini27 <hrithiksaini.working@gmail.com>
2022-05-29 17:42:52 +0530

    Test code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
-import React from "react";
-
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>Resnik Algorithm:</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab explicabo
+        molestiae nostrum dolorum amet et ullam quisquam deleniti, expedita,
+        omnis repellendus eum minima dignissimos voluptatem totam veniam esse
+        consectetur nihil.
+      </p>
+      <br />
+      <br />
+      <h1>Simple Lesk Algorithm:</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab explicabo
+        molestiae nostrum dolorum amet et ullam quisquam deleniti, expedita,
+        omnis repellendus eum minima dignissimos voluptatem totam veniam esse
+        consectetur nihil.
+      </p>
+    </>
+  );
 };
